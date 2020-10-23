80e99e0663aee5aa2f792fb7369ecae2b518723b
angellucruz16 <angellucruz16@hotmail.com>
2020-10-23 18:18:43 -0500

    COMMIT 2
    Cree 2 clases en  modelo

---
+++ src/modelo/Perro.java
@@ -0,0 +1,24 @@
+package modelo;
+
+public class Perro {
+
+	private String nombre;
+	private int id;
+	private int edad;
+	private String fecha;
+	private String raza;
+
+	public Perro (String nombre, int id, int edad, String fecha, String raza) {
+	this.nombre = nombre;
+	this.id = id;
+	this.edad = edad;
+	this.fecha = fecha;
+	this.raza = raza;
+	
+		
+		
+		
+		
+	}//CONSTRUCTOR
+
+} //CLASE
